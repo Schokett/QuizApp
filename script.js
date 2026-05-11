00b93413d9c9d9ce6bfd94b2d9adc37acbadf716
schokett <skrollanelenazimmermann@gmail.com>
2026-05-11 23:40:10 +0200

    updated questions

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 const questions = [
   {
     id: 1,
-    question: "Was ist die Haupstadt von Deutschland",
+    question: "Auf welcher japanischen Insel liegt die Hauptstadt Tokio?",
     options: [
-      { id: "a", text: "Hamburg", correct: false },
-      { id: "b", text: "Berlin", correct: true },
-      { id: "c", text: "München", correct: false },
-      { id: "d", text: "Hannover", correct: false },
+      { id: "a", text: "Hokkaido", correct: false },
+      { id: "b", text: "Honshu", correct: true },
+      { id: "c", text: "Kyushu", correct: false },
+      { id: "d", text: "Shikoku", correct: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "Was beschreibt der Begriff 'Sakura' in der japanischen Kultur?",
+    options: [
+      { id: "a", text: "Die Kunst des Papierfaltens", correct: false },
+      { id: "b", text: "Die traditionelle Teezeremonie", correct: false },
+      { id: "c", text: "Die Kirschblüte", correct: true },
+      { id: "d", text: "Ein scharfes Gewürz aus Meerrettich", correct: false },
     ],
   },
   {
     id: 3,
-    question: "Welcher Fluss fließt durch Hamburg?",
+    question: "Wie heißt der höchste Berg Japans, der auch ein heiliges Symbol ist?",
     options: [
-      { id: "a", text: "Rhein", correct: false },
-      { id: "b", text: "Donau", correct: false },
-      { id: "c", text: "Elbe", correct: true },
-      { id: "d", text: "Weser", correct: false },
+      { id: "a", text: "Mount Everest", correct: false },
+      { id: "b", text: "Mount Kita", correct: false },
+      { id: "c", text: "Mount Fuji", correct: true },
+      { id: "d", text: "Mount Aso", correct: false },
     ],
   },
   {
     id: 4,
-    question: "Wie viele Bundesländer hat Deutschland?",
+    question: "Welches Gericht besteht traditionell aus gesäuertem Reis und oft rohem Fisch?",
     options: [
-      { id: "a", text: "12", correct: false },
-      { id: "b", text: "14", correct: false },
-      { id: "c", text: "16", correct: true },
-      { id: "d", text: "18", correct: false },
+      { id: "a", text: "Ramen", correct: false },
+      { id: "b", text: "Sashimi", correct: false },
+      { id: "c", text: "Sushi", correct: true },
+      { id: "d", text: "Tempura", correct: false },
     ],
   },
   {
     id: 5,
-    question: "Was ist das chemische Symbol für Wasser?",
+    question: "Wie nennt man die japanischen Ritter des Adelsstandes im vorindustriellen Japan?",
     options: [
-      { id: "a", text: "O2", correct: false },
-      { id: "b", text: "H2O", correct: true },
-      { id: "c", text: "CO2", correct: false },
-      { id: "d", text: "HO", correct: false },
+      { id: "a", text: "Ninja", correct: false },
+      { id: "b", text: "Samurai", correct: true },
+      { id: "c", text: "Shogun", correct: false },
+      { id: "d", text: "Ronin", correct: false },
     ],
   },
 ];
@@ -46,10 +56,10 @@ function startDisplay() {
   const startBody = `
   <h2 id="question-header"></h2>
       <div class="answer-container">
-        <button id="btn-one"></button>
-        <button id="btn-two"></button>
-        <button id="btn-three"></button>
-        <button id="btn-four"></button>
+        <button id="btn-one" onclick="checkAnwer(btn-one)"></button>
+        <button id="btn-two" onclick="checkAnwer(btn-two)"></button>
+        <button id="btn-three" onclick="checkAnwer(btn-three)"></button>
+        <button id="btn-four" onclick="checkAnwer(btn-four)"></button>
       </div>`;
   const quizBody = document.getElementById("quiz-frame");
   quizBody.innerHTML = startBody;
@@ -57,6 +67,7 @@ function startDisplay() {
 function endDisplay() {
   const startBody = `
   <h2 id="question-header">Du hast alle Fragen durch!</h2>
+   <h2 id="question-header">Klicke auf weiter zum neustarten.</h2>
   `;
   const quizBody = document.getElementById("quiz-frame");
   quizBody.innerHTML = startBody;
@@ -87,6 +98,22 @@ function newQuestion() {
     questionsIndex++;
   } else {
     endDisplay();
+    questionsIndex = 0;
   }
-  console.log(questionsIndex);
+}
+function checkAnwer(answerId) {
+  const btnOne = document.getElementById("btn-one");
+  const btnTwo = document.getElementById("btn-two");
+  const btnThree = document.getElementById("btn-three");
+  const btnFour = document.getElementById("btn-four");
+  const selectedButton = document.getElementById(answerId);
+  if (questions[questionsIndex].options[answerId].correct === true) {
+    selectedButton.classList.add("true");
+    console.log("Richtig geantwortet!");
+  } else {
+    selectedButton.classList.add("false");
+    console.log("Leider falsch.");
+  }
+
+  console.log("Gewählte ID:", answerId);
 }
