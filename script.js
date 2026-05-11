47b83769f4bc0f8999c9ec9893ef4aa2a0c212f4
schokett <skrollanelenazimmermann@gmail.com>
2026-05-11 13:16:58 +0200

    objekt updated

---
+++ script.js
@@ -0,0 +1,21 @@
+const questions = [
+  {
+    id: 1,
+    questions: "Was ist die Haupstadt von Deutschland",
+    options: [
+      { id: "a", text: "Hamburg", correct: false },
+      { id: "b", text: "Berlin", correct: true },
+      { id: "c", text: "München", correct: false },
+      { id: "d", text: "Hannover", correct: false },
+    ],
+  },
+  {
+    id: 2,
+    questions: "Der Die oder Das : Baum",
+    options: [
+      { id: "a", text: "der", correct: true },
+      { id: "b", text: "die", correct: false },
+      { id: "c", text: "das", correct: false },
+    ],
+  },
+];
